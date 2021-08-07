0bf5c43b81ca5184873df571c0b259e32bebfea0
Luiz <luiz10hdsg@gmail.com>
2021-08-07 17:12:09 -0300

    Commit9

--- Movie.java
+++ Movie.java
@@ -23,5 +23,25 @@ public class Movie {
     public String getTitle (){
         return _title;
     }
+
+    public double getCharge(int daysRented){
+        
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2)
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount +=getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3)
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                break;
+         }
+    }
+
   }
   
