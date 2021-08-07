51b9b94c101ff0154f7ee57c0a575818dd3fc170
Luiz <luiz10hdsg@gmail.com>
2021-08-07 17:16:19 -0300

    Commit10

--- Rental.java
+++ Rental.java
@@ -17,25 +17,12 @@ public class Rental {
    }
 
    public double getCharge(){
-      //Adicionar o trecho de código extraído.
-     // double thisAmount = 0;
-      switch (getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-             thisAmount += 2;
-             if (getDaysRented() > 2)
-                 thisAmount += (getDaysRented() - 2) * 1.5;
-             break;
-         case Movie.NEW_RELEASE:
-             thisAmount +=getDaysRented() * 3;
-             break;
-         case Movie.CHILDRENS:
-             thisAmount += 1.5;
-             if (getDaysRented() > 3)
-                 thisAmount += (getDaysRented() - 3) * 1.5;
-             break;
-      }
+      
+     return _movie.getCharge(_daysRented);
+      
    }
    public int getFrequentRenterPoints() {
-      frequentRenterPoints ++;
+      return _movie.getFrequentRenterPoints(_daysRented);
+      
    }
 }
