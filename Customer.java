68c6e42d293e548a2f30fccfa50d4cf5ba834482
Luiz <luiz10hdsg@gmail.com>
2021-08-07 17:01:29 -0300

    Commit7

--- Customer.java
+++ Customer.java
@@ -18,31 +18,43 @@ public class Customer {
    }
   
   public String statement() {
-     double totalAmount = 0;
-     int frequentRenterPoints = 0;
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
+        
         Rental each = (Rental) rentals.nextElement();
         
-        // add frequent renter points
-        frequentRenterPoints ++;
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1) frequentRenterPoints ++;
-
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += each.getCharge();
+            String.valueOf(each.getCharge()) + "\n";
+        
 
      }
      //add footer lines
-     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-     result += "You earned " + String.valueOf(frequentRenterPoints) +
+     result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+     result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
              " frequent renter points";
      return result;
    }
+
+   private double getTotalCharge() {
+    double result = 0;
+    Enumeration rentals = _rentals.elements();
+    while (rentals.hasMoreElements()) {
+       Rental each = (Rental) rentals.nextElement();
+       result += each.getCharge();
+       }
+       return result;
+ }
+
+ private int getTotalFrequentRenterPoints(){
+    int result = 0;
+    Enumeration rentals = _rentals.elements();
+    while (rentals.hasMoreElements()) {
+       Rental each = (Rental) rentals.nextElement();
+       result += each.getFrequentRenterPoints();
+    }
+    return result;
+ }
 
 }
